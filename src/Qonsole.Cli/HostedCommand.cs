a340c6a8927cba1c4fe1bd928eea6d4e70df0e4d
Marc Vitalis <marc.vitalis@live.com>
2022-07-14 13:54:48 +0800

    working hosted service

--- src/Qonsole.Cli/HostedCommand.cs
+++ src/Qonsole.Cli/HostedCommand.cs
@@ -1,4 +1,8 @@
 using System.CommandLine;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
 
 namespace Qonsole.Cli;
 
@@ -7,26 +11,47 @@ public class HostedCommand : Command
     public HostedCommand()
         : base("hosted", "Hosted command demo")
     {
-		var sourceOption = new Option<string>(
-			name: "--source",
-			description: "Source file");
+        var sourceOption = new Option<string>(
+            aliases: new string[] { "--source", "-s" },
+            description: "Source file");
 
-		this.AddOption(sourceOption);
+        this.AddOption(sourceOption);
 
-		var nameOption = new Option<string>(
-			name: "--name",
-			description: "Name of action to be loaded");
+        var nameOption = new Option<string>(
+            aliases: new string[] { "--name", "-n" },
+            description: "Name of action to be loaded");
 
-		this.AddOption(nameOption);
+        this.AddOption(nameOption);
 
-		this.SetHandler(async (source, name) =>
-			await ExecuteAsync(source, name),
-			sourceOption, nameOption);
+        var targetOption = new Option<string>(
+            aliases: new string[] { "--target", "-t" },
+            description: "The target extra parameter");
+
+        this.AddOption(targetOption);
+
+        this.SetHandler(async (source, name, target) =>
+            await ExecuteAsync(source, name, target),
+            sourceOption, nameOption, targetOption);
     }
 
-	public async Task ExecuteAsync(string source, string name)
-	{
-		await Console.Out.WriteLineAsync(source);
-		await Console.Out.WriteLineAsync(name);
-	}
+    public async Task ExecuteAsync(string source, string name, string target)
+    {
+        var assembly = Assembly.LoadFrom(source);
+		var type = assembly.GetTypes().FirstOrDefault(t => t.Name == name);
+
+        //rehydrate args
+        string[] args = { $"target={target}" };
+
+        IHost host = Host.CreateDefaultBuilder(args)
+			.ConfigureServices((hostContext, services) =>
+			{
+				if(type != null)
+				{
+					services.TryAddEnumerable(ServiceDescriptor.Singleton(typeof(IHostedService), type));
+				}
+			})
+            .Build();
+
+        await host.RunAsync();
+    }
 }
