597cec3c380b5411403618226f5041fe6dca3bcb
Marc Vitalis <marc.vitalis@live.com>
2023-02-04 02:10:17 +0800

    sample enhancements

--- src/Qonsole.Sample/Program.cs
+++ src/Qonsole.Sample/Program.cs
@@ -1,17 +1,37 @@
 ﻿using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Parsing;
 
 // See https://aka.ms/new-console-template for more information
 
 var root = new RootCommand("Hello world sample application");
 
-root.Add(
+var nameOption = 
     new Option<string>(
         aliases: new string[] { "--name", "-n" },
         description: "Name to greet"
-    ));
+    );
+
+root.Add(nameOption);
 
 root.SetHandler((name) =>
-    Console.WriteLine($"Hello, {name}!!!")
+    Console.WriteLine($"Hello, {name}!!!"),
+    nameOption
 );
 
-await root.InvokeAsync(args);
+var help = new Command("output-help", "")
+{
+    IsHidden = true
+};
+
+root.AddCommand(help);
+
+var builder = new CommandLineBuilder(root);
+
+builder.UseDefaults();
+
+var parser = builder.Build();
+
+Console.WriteLine(string.Join(" ", args));
+
+await parser.InvokeAsync(args);
